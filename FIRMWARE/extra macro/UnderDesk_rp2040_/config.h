0e2c3c1ab2c32cb353bb1c63445bc11d8ad63b2d
juarendra <jrjuarendra@gmail.com>
2024-11-16 22:37:26 +0700

    FEAT: add Firmware extra macro

---
+++ FIRMWARE/extra macro/UnderDesk_rp2040_/config.h
@@ -0,0 +1,12 @@
+#pragma once
+#define DYNAMIC_KEYMAP_MACRO_COUNT 32
+#define DEBOUNCE 5
+
+#define ENCODERS_PAD_A {GP10}
+#define ENCODERS_PAD_B {GP9}
+#define ENCODER_DIRECTION_FLIP
+#define ENCODER_RESOLUTIONS {4}
+
+#define WS2812_DI_PIN GP11 //D3 - underglow C7 - backlight
+#define RGBLED_NUM 1    // Number of L
+#define MIDI_BASIC
